03d472d1abf8831acd5a7da9bf1edb2e7d7d04de
Dane <nguyenkhanhdanh170201@gmail.com>
2024-01-18 11:23:52 +0700

    danh

--- resources/views/components/frontend/frontend-links.blade.php
+++ resources/views/components/frontend/frontend-links.blade.php
@@ -9,7 +9,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/6172dca7f7c0440a591f8a28/1fikarru0';
+
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
